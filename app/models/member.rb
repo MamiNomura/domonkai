d7cced47d1e55bdfca4ab9c7798ed6aed1022d79
Mami Nomura <mami.nomura@blackbaud.com>
2015-05-20 16:15:47 -0700

    Added more attributes to excel sheet

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,5 +3,11 @@ class Member < ActiveRecord::Base
   has_many :students, :class_name => "Member", :foreign_key => "sensei_member_id"
   belongs_to :shikaku_kubun
 
-
+  validates :phone, numericality: { only_integer: true }
+  validates :fax, numericality: { only_integer: true }
+  validates :domonkai_id,  presence: true, uniqueness: true
+  validates :first_name,  presence: true
+  validates :last_name,  presence: true
+  validates :japanese_first_name,  presence: true
+  validates :japanese_last_name,  presence: true
 end
